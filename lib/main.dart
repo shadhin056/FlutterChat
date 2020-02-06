bb28e6898447fb41a6d116b5a1b8ebb812cba3f1
ShadhiN <shadhinemail@gmail.com>
2020-02-06 22:39:13 +0600

    init with route

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat/page/welcome.dart';
+import 'package:chat/page/chat.dart';
+import 'package:chat/page/login.dart';
+import 'package:chat/page/registration.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
